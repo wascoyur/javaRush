93a66449efe88387457081f3f32e9d777da05a50
wascoyur <mytem20007@gmail.com>
2016-06-19 21:08:46 +0500

    решено

--- src/com/javarush/test/level16/lesson13/home02/Solution.java
+++ src/com/javarush/test/level16/lesson13/home02/Solution.java
@@ -2,7 +2,8 @@ package com.javarush.test.level16.lesson13.home02;
 
 /* Последовательные выполнения нитей
 1. В методе run после всех действий поставь задержку в 10 миллисекунд. Выведи "Нить прервана", если нить будет прервана.
-2. Сделай так, чтобы все нити выполнялись последовательно: сначала для нити №1 отсчет с COUNT до 1, потом для нити №2 с COUNT до 1 и т.д.
+2. Сделай так, чтобы все нити выполнялись последовательно:
+сначала для нити №1 отсчет с COUNT до 1, потом для нити №2 с COUNT до 1 и т.д.
 Пример:
 #1: 4
 #1: 3
@@ -17,8 +18,10 @@ public class Solution {
 
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < COUNT; i++) {
-            new SleepingThread();
+            Thread net = new SleepingThread();
             //напишите тут ваш код
+            net.join();
+
         }
     }
 
@@ -26,9 +29,11 @@ public class Solution {
         private volatile int countDownIndex = COUNT;
         private static volatile int threadCount = 0;
 
-        public SleepingThread() {
+        public SleepingThread() throws InterruptedException {
             super(String.valueOf(++threadCount));
             start();
+
+
         }
 
         public void run() {
@@ -36,6 +41,12 @@ public class Solution {
                 System.out.println(this);
                 if (--countDownIndex == 0) return;
                 //add sleep here - добавь sleep тут
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    System.out.println("Нить прервана");;
+                }
+
             }
         }
 
