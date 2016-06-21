472b2c9f59dcb9eb075ba48fa07aba23e4b330ed
wascoyur <mytem20007@gmail.com>
2016-06-21 18:32:32 +0500

    предыдущая задача похожа

--- src/com/javarush/test/level16/lesson13/home03/Solution.java
+++ src/com/javarush/test/level16/lesson13/home03/Solution.java
@@ -3,11 +3,14 @@ package com.javarush.test.level16.lesson13.home03;
 /* Рекурсивное создание нитей
 +1. Измени класс GenerateThread так, чтобы он стал нитью.
 2. Создай конструктор GenerateThread, который должен:
-2.1. Вызвать конструктор суперкласса с параметром String - номером созданной нити. Используй countCreatedThreads.
+2.1. Вызвать конструктор суперкласса с параметром String - номером созданной нити.
+Используй countCreatedThreads.
 2.2. Запустить текущую нить.
 2.3. Номер первой нити должен начинается с 1.
-3. Переопредели метод toString, для этого внутри GenerateThread нажми Alt+Insert -> Override Methods. Начни печатать toString.
-3.1. Метод toString должен возвращать № текущей нити и слово " created". Используй getName(). Пример: [8 created]
+3. Переопредели метод toString, для этого внутри GenerateThread нажми Alt+Insert -> Override Methods.
+Начни печатать toString.
+3.1. Метод toString должен возвращать № текущей нити и слово " created". Используй getName().
+Пример: [8 created]
 4. Пока количество созданных нитей меньше Solution.count метод run должен:
 4.1. создать новую нить типа GenerateThread.
 4.2. Вывести в консоль созданную в пункте 4.1. нить.
@@ -16,20 +19,31 @@ package com.javarush.test.level16.lesson13.home03;
 
 public class Solution {
     static int count = 15;
-    static volatile int countCreatedThreads;
+    static volatile int countCreatedThreads ;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(new GenerateThread());
     }
 
     public static class GenerateThread extends Thread{
-        public GenerateThread(String name) {
-            super(name);
-            run();
+
+
+        public GenerateThread() throws InterruptedException {
+            super(String.valueOf(++countCreatedThreads));
+            start();
+
+        }
+        @Override
+        public void run() {
+            for (int i = 0; i <= countCreatedThreads; i++) {
+                start();
+                System.out.println(currentThread());
+            }
         }
 
-        public GenerateThread() {
-
+        @Override
+        public String toString() {
+            return  getName()+ " created";
         }
     }
 }
