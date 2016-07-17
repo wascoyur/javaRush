0a12f1f4c9de9b5c0993c23660031a4b6bc3ab96
wascoyur <mytem20007@gmail.com>
2016-07-17 21:56:52 +0500

    Печать должна быть здесь, т.е. остальной код только формирует данные, не печатает.

---
+++ src/com/javarush/test/level23/lesson13/big01/Room.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level23.lesson13.big01;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vasiliev on 17.07.2016.
+ */
+public class Room {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by vasiliev on 17.07.2016.
+ */
+public interface SelfInterfaceMarker {
+}
