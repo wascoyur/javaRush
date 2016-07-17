934e382dd836512e3fc83f955835c8aaed6cda16
wascoyur <mytem20007@gmail.com>
2016-07-17 21:28:07 +0500

    save

---
+++ src/com/javarush/test/level24/lesson04/task01/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level24.lesson04.task01;
+
+import java.math.BigDecimal;
+
+/* Наследование от внутреннего класса
+Внутри класса Solution создайте 2 внутренних public класса Apt3Bedroom, BigHall.
+Унаследуйте их от Apartments и Hall.
+*/
+public class Solution {
+
+    public class Building {
+        public class Hall {
+            private BigDecimal square;
+
+            public Hall(BigDecimal square) {
+                this.square = square;
+            }
+            public class BigHall extends Building.Hall{
+                BigHall(Building building) {
+                    building.super(square);
+                }
+            }
+        }
+
+        public class Apartments {
+            public class Apt3Bedroom extends Apartments{
+
+            }
+        }
+    }
+
+}
