cb8979de9b4a2ab9fa465a55dbca31744b250c05
wascoyur <mytem20007@gmail.com>
2016-07-20 12:35:56 +0500

    save

--- src/com/javarush/test/level22/lesson18/big01/Field.java
+++ src/com/javarush/test/level22/lesson18/big01/Field.java
@@ -25,7 +25,12 @@ public class Field {
     public int[][] getMatrix() {
         return matrix;
     }
-    void print(){};// - объект будет отрисовывать на экран свое текущее состояние;
+    void print(){// - объект будет отрисовывать на экран свое текущее состояние;
+        int height = 0; int width = 0;
+        if (matrix[y][x] == 0) {
+
+        }
+    }
     void removeFullLines(){};// - будет удалять из матрицы полностью заполненные строки и сдвигать вышележащие строки вниз;
     Integer getValue(int x, int y){return null;}// - возвращает значение которое находится в матрице с координатами x и y;
     void setValue(int x, int y, int value){}// - устанавливает переданное значение в ячейку массива (матрицы
--- src/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -4,4 +4,34 @@ package com.javarush.test.level22.lesson18.big01;
  * Created by vasiliev on 20.07.2016.
  */
 public class Figure {
+    int x;
+    int y;
+    int[][] matrix;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Figure(int x, int y, int[][] matrix) {
+
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+    public void left(){};// - для движения фигурки влево.
+    public void right(){};// - для движения фигурки вправо.
+    public void down(){};// - для движения фигурки вниз.
+    public void up(){};// - для движения фигурки вверх.
+    public void downMaximum(){};// - падение фигурки в низ до дна.
+    public void rotate(){};// - для поворота фигурки вокруг главной диагонали.
+    public boolean isCurrentPositionAvailable(){return true;};// - проверка - может ли фигурка быть помещена в текущую позицию. Для теста захардкодь результат в true.
+    public void landed(){};// - вызывается, когда фигурка достигла дна или уперлась в другую фигурку
 }
