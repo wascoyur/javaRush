1818f9914abbbadc0c6d474ed2284ff2ab0378a5
wascoyur <mytem20007@gmail.com>
2016-07-29 11:06:54 +0500

    home

--- src/com/javarush/test/level17/lesson10/home02/Beach.java
+++ src/com/javarush/test/level17/lesson10/home02/Beach.java
@@ -4,7 +4,9 @@ package com.javarush.test.level17.lesson10.home02;
 Реализуйте интерфейс Comparable<Beach> в классе Beach, который будет использоваться нитями.
 */
 
-public class Beach implements Comparable{
+import java.util.Comparator;
+
+public class Beach implements Comparable {
     private String name;      //название
     private float distance;   //расстояние
     private int quality;    //качество
@@ -20,17 +22,15 @@ public class Beach implements Comparable{
     }
 
     public void setName(String name) {
-        synchronized (this){
-            this.name = name;}
+        this.name = name;
     }
 
     public float getDistance() {
         return distance;
     }
 
-    public void  setDistance(float distance) {
-        synchronized (this){
-        this.distance = distance;}
+    public void setDistance(float distance) {
+        this.distance = distance;
     }
 
     public int getQuality() {
@@ -38,17 +38,11 @@ public class Beach implements Comparable{
     }
 
     public void setQuality(int quality) {
-        synchronized (this) {
-            this.quality = quality;
-        }
+        this.quality = quality;
     }
 
     @Override
-    public synchronized int compareTo(Object o) {
-        Beach tmp = (Beach)o;
-        int distanceParam = (int) (this.distance - tmp.getDistance());
-        int qualityParam = this.quality - tmp.getQuality();
-        return 10000 * name.compareTo(tmp.getName()) + 100 * distanceParam + qualityParam;
-
+    public int compareTo(Object o) {
+        return 0;
     }
 }
