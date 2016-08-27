2f45954a589ae65564e0d87fb4428343a0e77612
wascoyur <mytem20007@gmail.com>
2016-08-27 12:45:43 +0500

    save work

---
+++ src/com/javarush/test/level20/lesson02/task05/tmp/Cat.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level20.lesson02.task05.tmp;
+
+/**
+ * Created by vasiliev on 23.08.2016.
+ */
+public class Cat {
+}
---
+++ src/com/javarush/test/level20/lesson07/task05/tmp/PersistentTime.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level20.lesson07.task05.tmp;
+
+/**
+ * Created by vasiliev on 26.08.2016.
+ */
+public class PersistentTime {
+}
