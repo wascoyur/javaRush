51f8545ab52f8e8b3d70275b3f394a3517f26467
wascoyur <mytem20007@gmail.com>
2016-09-18 11:27:49 +0500

    списана 25/9/03

--- src/com/javarush/test/level19/lesson03/task04/Solution.java
+++ src/com/javarush/test/level19/lesson03/task04/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.test.level19.lesson03.task04;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Date;
 import java.util.Scanner;
 
 /* И еще один адаптер
@@ -12,37 +9,11 @@ import java.util.Scanner;
 Данные в файле хранятся в следующем виде:
 Иванов Иван Иванович 31 12 1950
 
-В файле хранится большое количество людей, данные одного человека
-находятся в одной строке. Метод read() должен читать данные одного человека.
+В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен читать данные одного человека.
 */
 
 public class Solution {
-    public static class PersonScannerAdapter implements PersonScanner {
-        private Scanner scanner;
-        public PersonScannerAdapter(Scanner scanner) {
-            this.scanner = scanner;
-        }
-
-        @Override
-        public Person read() throws IOException {
-            BufferedReader br = new BufferedReader(new FileReader("d:\\f1"));
-            String s = br.readLine();
-            String firstName = scanner.next(s);
-            String middleName = scanner.next(s);
-            String lastName = scanner.next(s);
-            Date birthDate = new Date(scanner.next(s));
-
-            return new Person(firstName,middleName,lastName,birthDate);
-        }
-
-        @Override
-        public void close() throws IOException {
-
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
+    public static class PersonScannerAdapter {
 
     }
 }
